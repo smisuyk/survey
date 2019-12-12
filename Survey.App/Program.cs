ba3a18fafb2af07b70c57e1a37a01d5765bcbddc
smisuyk <smisuyk@mail.ru>
2019-12-12 22:13:12 +0300

    program fixed

--- Survey.App/Program.cs
+++ Survey.App/Program.cs
@@ -11,37 +11,42 @@ namespace Survey.App
         static void Main(string[] args)
 
         {
-            int a = 0;
+            string a;
+            int o = 0;
             Console.WriteLine("qoestion!");
-            string[] rightAnswer = new string[5] { "50", "Master of Sports", "Simpleo", "1964", "Tokyo" };
-            string[] question = new string[5] { "How long did dinosaurs grow?", "How is kmc decoded?", "who is the top 1 of the world by cs go?", "what year did the porsche 911 appear?", "The capital of Japan?" };
-            for (int i = 0; i < question.Length; i++)
-            {
-                Console.WriteLine(question[i]);
-                Console.WriteLine();
-                if (question[0] == rightAnswer[0])
-                {
-                    Console.WriteLine("You are right!", a++);
-                    Console.WriteLine(question[1]);
-                };
-                Console.Read();
-
-            }
-        
-
-            return;
-            Console.WriteLine();
-            if (question[0] == rightAnswer[0]) {
-                Console.WriteLine("You are right!");
-          
-            }
-
+            string[] rightAnswer = new string[] { "50", "Master of Sports", "Simpleo", "1964", "Tokyo","printer ", "Isaac Newton","programming", "exe", "Марк Цукерберг" };
+            string[] question = new string[] { "How long did dinosaurs grow?",
+                "How is kmc decoded?",
+                "who is the top 1 of the world by cs go?",
+                "what year did the porsche 911 appear?",
+                "The capital of Japan?",
+                "Text printing device?",
+                "Which physicist apple fell on Newton’s head?",
+                "C #, java, c ++ are languages?",
+                "Windows executable?",
+                "who develope d facebook"};
+            
             for (int i = 0; i < question.Length; i++)
             {
                 Console.WriteLine(question[i]);
                 
+               a = Console.ReadLine();
+                if (rightAnswer[i] == a) {
+                    o++;
+                    
+                
+                }
+                
             }
-            
+            Console.WriteLine("percentage of correct answers");
+            o = o * 100 / 10;
+            Console.WriteLine(o);
+           
+
+
+
+
+
 
         }
 
