aba938642a8d46b38d5fe26e1ca61fabbd525738
smisuyk <smisuyk@mail.ru>
2019-12-13 19:17:53 +0300

    problem
    пишет 2 раза один и тот же текс

--- Survey.App/Program.cs
+++ Survey.App/Program.cs
@@ -9,15 +9,15 @@ namespace Survey.App
     class Program
     {
         static void Main(string[] args)
-
         {
             string a;
             int o = 0;
-            int j;
-            int i = 0;
+            int j=0;
+            int i;
             Console.WriteLine("qoestion!");
-            string[] rightAnswer = new string[] { "50", "Master of Sports", "Simpleo", "1964", "Tokyo","printer ", "Isaac Newton","programming", "exe", "Марк Цукерберг" };
-            
+             int[] alredyansswerquestio = new   int[5];
+            string[] rightAnswer = new string[] { "50", "Master of Sports", "Simpleo", "1964", "Tokyo", "printer ", "Isaac Newton", "programming", "exe", "Марк Цукерберг" };
+
             string[] question = new string[] { "How long did dinosaurs grow?",
                 "How is kmc decoded?",
                 "who is the top 1 of the world by cs go?",
@@ -28,39 +28,29 @@ namespace Survey.App
                 "C #, java, c ++ are languages?",
                 "Windows executable?",
                 "who develope d facebook"};
-            Random r = new Random();
-           
-           
-
-                for (i = 0; i < question.Length; i++)
-                {
-                    j = r.Next(1, 10);
-                    Console.WriteLine(question[j]);
-
-                    a = Console.ReadLine();
-                    
-                    if (rightAnswer[j] == a) {
-                        o++;
-
-
-                    }
-                } 
             
+            Random random = new Random();
+            for (i = 0; i < question.Length; i++)
+            {
+
+                var randomindex= random.Next(0, 10);
+                while (alredyansswerquestio.Contains(randomindex)) ;
+                { randomindex = random.Next(0, 10); }
+                alredyansswerquestio[i] = randomindex;
+                Console.WriteLine(question[j]);
+
+                a = Console.ReadLine();
+
+                if (rightAnswer[j] == a)
+                {
+                    o++;
+
+
+                }
+            }
             Console.WriteLine("percentage of correct answers");
             o = o * 100 / 10;
             Console.WriteLine(o);
-           
-
-
-
-
-
-
         }
-
-
-
-
-        
     }
 }
