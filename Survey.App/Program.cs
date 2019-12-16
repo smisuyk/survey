5c1178889b92d091e48dde47ded34e5a197747c7
smisuyk <smisuyk@mail.ru>
2019-12-16 18:48:24 +0300

    Данный формат пути не поддерживается.

--- Survey.App/Program.cs
+++ Survey.App/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,28 +15,24 @@ namespace Survey.App
             int o = 0;
             int i;
             Console.WriteLine("question!");
-             int[] alredyansswerquestio = new int[5];
-            string[] rightAnswer = new string[] { "50", "Master of Sports", "Simpleo", "1964", "Tokyo", "printer", "Isaac Newton", "programming", "exe", "Марк Цукерберг" };
-            string[] question = new string[] { "How long did dinosaurs grow?",
-                "How is kmc decoded?",
-                "who is the top 1 of the world by cs go?",
-                "what year did the porsche 911 appear?",
-                "The capital of Japan?",
-                "Text printing device?",
-                "Which physicist apple fell on Newton’s head?",
-                "C #, java, c ++ are languages?",
-                "Windows executable?",
-                "who develope d facebook"};
-            
+            int[] alredyansswerquestio = new int[5];
+
+            var question = File.ReadAllLines(@"‪D:\dev\survey\survey\Survey.App\question.txt");
+            var rightAnswer = File.ReadAllLines(@"‪D:\dev\survey\survey\Survey.App\rightAnswer.txt");
+    
             Random random = new Random();
             for (i = 0; i < 5; i++)
             {
-                var randomindex= random.Next(0, 10);
-                while (alredyansswerquestio.Contains(randomindex)) 
-                { randomindex = random.Next(0, 10); }
+                var randomindex = random.Next(0, 10);
+                while (alredyansswerquestio.Contains(randomindex))
+                { 
+                    randomindex = random.Next(0, 10);
+                }
+                
                 alredyansswerquestio[i] = randomindex;
                 Console.WriteLine(question[randomindex]);
                 a = Console.ReadLine();
+
                 if (rightAnswer[randomindex] == a)
                 {
                     o++;
